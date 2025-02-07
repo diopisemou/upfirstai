4a1780804a2089902030b9d2b144f5dd3a5178ac
diopisemou <diopisemou@gmail.com>
2025-02-07 04:31:40 +0000

    minor changes - terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,12 +6,16 @@ variable "region" {
 
 variable "ecr_repository_url" {
   description = "AWS Elastic Container Registery Url"
+  type = string
+  sensitive = true
+  default = "586794453336.dkr.ecr.us-west-2.amazonaws.com/"
 }
 
 variable "jwt_secret" {
   description = "JWT secret key"
   type        = string
   sensitive   = true
+  default = "mysecret"
 }
 
 resource "aws_ecr_repository" "upfirstai_api_ts" {
@@ -27,9 +31,16 @@ resource "aws_ecr_repository" "upfirstai_api_ts" {
 variable "subnets" {
   description = "Subnets for ECS service"
   type        = list(string)
+  default = [
+    "subnet-003398478c7b13c75",
+    "subnet-0f1328fbee3eb3dca",
+    "subnet-0faa1734d64eded50",
+    "subnet-0f349344389d7c4c6"
+  ]
 }
 
 variable "security_groups" {
   description = "Security groups for ECS service"
   type        = list(string)
+  default = ["sg-006ba7e79867d12e5"]
 }
